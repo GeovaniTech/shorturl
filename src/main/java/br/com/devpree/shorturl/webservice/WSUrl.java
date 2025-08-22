c4c09cfaaf760df4e3e5843889523420550dd5cf
Geovani Debastiani <geovanidebastiani32@gmail.com>
2025-08-21 22:23:08 -0300

    fix: Ajusting endpoint from POST TO PUT
    Issue: https://github.com/GeovaniTech/shorturl/issues/9

--- src/main/java/br/com/devpree/shorturl/webservice/WSUrl.java
+++ src/main/java/br/com/devpree/shorturl/webservice/WSUrl.java
@@ -15,7 +15,7 @@ import br.com.devpree.shorturl.util.StringUtil;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
@@ -61,7 +61,7 @@ public class WSUrl implements Serializable {
 	 * @throws Exception
 	 */
 	@Path("/create")
-	@POST
+	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response create(TOCreateUrlRequestRestModel request) throws Exception { 
